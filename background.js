13a0f464ede183273e84cfbdca6d65415f918745
liyd <tianlelyd@gmail.com>
2023-10-18 18:47:33 +0800

    修复快捷键必须要打开扩展界面时才能执行成功的错误！

--- background.js
+++ background.js
@@ -1,9 +1,26 @@
-chrome.commands.onCommand.addListener(function(command) {
-    if (command === "enable-all") {
-        // 启用所有插件的代码
-        chrome.runtime.sendMessage({action: "enableOrDisableAll", enable: true});
-    } else if (command === "disable-all") {
-        // 禁用所有插件的代码
-        chrome.runtime.sendMessage({action: "enableOrDisableAll", enable: false});
-    }
+chrome.commands.onCommand.addListener(function (command) {
+  if (command === "enable-all") {
+    // 启用所有插件的代码
+    //chrome.runtime.sendMessage({action: "enableOrDisableAll", enable: true});
+    enableOrDisableAll(true);
+  } else if (command === "disable-all") {
+    // 禁用所有插件的代码
+    // chrome.runtime.sendMessage({action: "enableOrDisableAll", enable: false});
+    enableOrDisableAll(false);
+  }
 });
+
+// 启用或禁用所有插件。
+function enableOrDisableAll(enable) {
+  chrome.management.getAll(function (extensions) {
+    extensions.forEach((extension) => {
+      // 不要启用或禁用自身。
+      if (extension.id !== chrome.runtime.id)
+        chrome.management.setEnabled(extension.id, enable,function() {
+            if (chrome.runtime.lastError) {
+                console.error(chrome.runtime.lastError.message);
+            } 
+        });
+    });
+  });
+}
